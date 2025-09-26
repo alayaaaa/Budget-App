0f9c5530d66daaa0fd4d828220b53da6019ab4df
Marion Julai Flores <119734359+alayaaaa@users.noreply.github.com>
2025-09-26 01:35:33 -0400

    Add files via upload

---
+++ src/Database.java
@@ -0,0 +1,80 @@
+import java.sql.*;
+import java.util.List;
+
+public class Database {
+
+    //Variables
+    private static final String DB_URL = "jdbc:sqlite:C:\\Users\\Marion\\Documents\\Budget App\\transactions.db";
+    private Connection connection;
+    
+    //Establishes a connection to transactions.db
+    //If file does not exist, create a new file with that file name
+    public Database() {
+
+        try {
+
+                this.connection = DriverManager.getConnection(DB_URL);
+                DatabaseMetaData meta = connection.getMetaData();
+
+                System.out.println("Connection to SQLite has been established.");
+                createTable();
+
+        } catch(SQLException e) {
+
+            System.err.println(e.getMessage());
+
+        }
+
+    }
+
+    //Creates a transaction table if it doesn't exist.
+    //Otherwise, just move on
+    public void createTable() {
+
+        String sql = "CREATE TABLE IF NOT EXISTS Transactions (" +
+                    " transactionID INTEGER PRIMARY KEY     AUTOINCREMENT, " +
+                    " Amount         REAL     NOT NULL, " +
+                    " Description    TEXT, " +
+                    " Category       TEXT    NOT NULL, " +
+                    " Date           TEXT)";
+
+        try (Statement stmt = connection.createStatement()){
+
+            stmt.execute(sql);
+
+        }catch(Exception e) {
+
+            System.out.println(e.getMessage());
+
+        }
+
+    }
+
+    //Inserts data from user to the database
+    public void insert(List<Transactions> transactions) {
+
+        String sql = "INSERT INTO Transactions (Amount, Description, Category, Date)" +
+                    " VALUES (?,?,?,?)"; 
+
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+
+            for(Transactions transaction : transactions) {
+
+                pstmt.setDouble(1, transaction.getAmount());
+                pstmt.setString(2, transaction.getDescription());
+                pstmt.setString(3, transaction.getCategory());
+                pstmt.setString(4, transaction.getDate().toString());
+
+                pstmt.executeUpdate();
+
+            }
+
+        }catch(Exception e) {
+
+            System.err.println(e.getMessage());
+
+        }
+
+    }
+
+}
