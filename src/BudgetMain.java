7fd3f75e003ef06d936e55ed49d40129f601c692
Marion Julai Flores <119734359+alayaaaa@users.noreply.github.com>
2025-09-26 01:38:17 -0400

    Update BudgetMain.java
    - Removed the entire JSON system
    - Replaced the JSON system with SQLite
    - Created a new java file to handle database queries
    - Added createTable method
    - Added insert method

--- src/BudgetMain.java
+++ src/BudgetMain.java
@@ -3,70 +3,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-
 public class BudgetMain {
-
-    //Save file method
-    //Appends to a JSON file
-    //WIP
-    public static void saveFile(List<Transactions> transactions) {
-
-        JSONParser parser = new JSONParser();
-        JSONArray transactionsList = new JSONArray();
-
-        // Read existing data from the file to a JSON array
-        try (FileReader reader = new FileReader("C:\\Users\\Marion\\Documents\\Budget App\\transactions.json")) {
-            
-            Object oldFile = parser.parse(reader);
-            transactionsList = (JSONArray) oldFile;
-
-        } catch (Exception e) {
-            
-            System.out.println("File not found or invalid. Creating a new JSON array...");
-            e.printStackTrace();
-
-        }
-
-        // Add transactions to the list
-        for(int i = 0; i <transactions.size(); i++) {
-
-            JSONObject transactionDetails = new JSONObject();
-
-            transactionDetails.put("amount", transactions.get(i).getAmount());
-            transactionDetails.put("description", transactions.get(i).getDescription());
-            transactionDetails.put("category", transactions.get(i).getCategory());
-            transactionDetails.put("date", transactions.get(i).getDate());
-
-            transactionsList.add(transactionDetails);
-
-        }
-        
-        // Creates a new file with the list, updates the list whenever needed
-        try(FileWriter file = new FileWriter("transactions.json")) {
-
-            file.write(transactionsList.toJSONString());
-            file.flush();
-
-            System.out.println("Succesfully copied JSON Object to a file.");
-            System.out.println("JSON Object: " + transactionsList);
-
-        }catch (IOException e) {
-
-            e.printStackTrace();
-
-        }
-
-    }
 
     public static void main(String[] args) {
 
         List<Transactions> transactions = new ArrayList<>();
+        Database db = new Database();
         Scanner scan = new Scanner(System.in);
         boolean enterTransaction = true;
 
@@ -101,25 +43,7 @@ public class BudgetMain {
         
         }
 
-        System.out.println("Do you want to save your file? (Y/N): ");
-        String saveFileConfirm = scan.next(); 
-
-        switch(saveFileConfirm) {
-
-                case "Y":
-                    saveFile(transactions);
-
-            }
-
-        double totalAmount = 0;
-
-            for(int i = 0; i < transactions.size(); i++) {
-
-                totalAmount += transactions.get(i).getAmount();
-
-            }
-
-            System.out.println(totalAmount);
+        db.insert(transactions);
 
         scan.close();
 
