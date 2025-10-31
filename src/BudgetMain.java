0fc69bb1820c76241200da955c02db4a4715358c
Marion Julai Flores <119734359+alayaaaa@users.noreply.github.com>
2025-10-30 23:31:05 -0400

    Update BudgetMain.java
    - Moved most functionalities to a separate java class called Shell.java

--- src/BudgetMain.java
+++ src/BudgetMain.java
@@ -1,51 +1,12 @@
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class BudgetMain {
 
     public static void main(String[] args) {
 
-        List<Transactions> transactions = new ArrayList<>();
         Database db = new Database();
-        Scanner scan = new Scanner(System.in);
-        boolean enterTransaction = true;
+        FinanceManager fm = new FinanceManager(db);
+        Shell shell = new Shell(db);
 
-        while(enterTransaction) {
-
-            System.out.println("Enter the amount: ");
-            while(!scan.hasNextDouble()) {
-
-                System.out.println("Not a valid input. Try Again.");
-                scan.next();
-
-            }
-            double amount = scan.nextDouble();
-
-            System.out.println("Enter the category: ");
-            String category = scan.next();
-
-            System.out.println("Enter a description (Optional): ");
-            String description = scan.next();
-
-            transactions.add(new Transactions(amount, category, description, LocalDate.now()));
-
-            System.out.println("Add more transactions? (Y/N): ");
-            String addTransaction = scan.next();
-
-            switch(addTransaction) {
-
-                case "N":
-                    enterTransaction = false;
-
-            }
-        
-        }
-
-        db.insert(transactions);
-
-        scan.close();
+        shell.start();
 
     }
 
